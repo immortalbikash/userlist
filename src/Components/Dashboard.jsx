f1648b0a9da18154ea4ea2faa79b7d9d07d35806
Bikash <bikashkatwalcr7@gmail.com>
2026-07-27 22:28:04 -0700

    fetch data form api in ui

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -5,30 +5,18 @@ import Userlist from './Userlist';
 
 const Dashboard = () => {
 
-    const [users, setUsers] = useState([]);
-    const [error, setError] = useState("");
-
-    const getUsers = async () => {
-
-        try {
-            const response = await axios.get(`https://jsonplaceholder.typicode.com/users`)
-            setUsers(response.data);
-        } catch (error) {
-            setError(error.message);
-        }
-
-
-        // console.log(response.data);
-    }
-
-    useEffect(() => {
-        getUsers();
-    }, [])
+  
 
     return (
         <div>
             <Userlist />
-            {
+            {/* {
+                users.map((user) => (
+                    <Userlist data={user} />
+                ))
+            }
+            <Userlist />
+            {/* {
                 users.map((user) => (
                     <div key={user.id}>
                         <h2>{user.name}</h2>
@@ -36,8 +24,8 @@ const Dashboard = () => {
                         <p>{user.address.city}</p>
                     </div>
                 ))
-            }
-            {error && <p>{error}</p>}
+            } */}
+            {/* {error && <p>{error}</p>} */}
         </div>
     )
 }
--- src/Components/Userlist.jsx
+++ src/Components/Userlist.jsx
@@ -1,22 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Userlist.css'
+import axios from 'axios';
 
 const Userlist = () => {
+
+    const [users, setUsers] = useState([]);
+    const [error, setError] = useState("");
+
+    const getUsers = async () => {
+
+        try {
+            const response = await axios.get(`https://jsonplaceholder.typicode.com/users`)
+            setUsers(response.data);
+            console.log(users);
+        } catch (error) {
+            setError(error.message);
+        }
+
+
+        // console.log(response.data);
+    }
+
+    useEffect(() => {
+        getUsers();
+    }, [])
+
     return (
         <div className='article'>
             <h1>Team Members</h1>
             <input type="text" placeholder='Search by name...' />
 
             <div className="user_list">
-                <div className="user">
-                    <div className="left">
-                        <h3>Leanne Graham</h3>
-                        <p>leane@gmail.com</p>
-                    </div>
-                    <div className="right">
-                        <p>Gwenborough</p>
-                    </div>
-                </div>
+                {
+                    users.map((user) => (
+                        <div key={user.id}>
+                            <div className="user">
+                                <div className="left">
+                                    <h3>{user.name}</h3>
+                                    <p>leane@gmail.com</p>
+                                </div>
+                                <div className="right">
+                                    <p>Gwenborough</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
             </div>
         </div>
     )
